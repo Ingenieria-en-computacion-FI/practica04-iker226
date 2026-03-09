da4d436b1f00bb67bdfd36947595d0d3a01676ea
Esquivel Cortés Iker Alonso <ikering226@gmail.com>
2026-03-09 13:09:37 -0600

    Update realloc.c

--- examples/realloc.c
+++ examples/realloc.c
@@ -3,28 +3,38 @@
 
 
 int main (){
-  // TODO(1): Declara dos apuntadores a entero llamados array, array2
-  // TODO(2): Declara una variable para indicar cuantos elementos se requieren
-  
-  printf("¿Cuantos elementos tiene el conjunto?\n");
-  // TODO(3): Lee el numero de elementos
-  // TODO(4): Usando una llamada a malloc reserva la memoria para array
-  
-  if (array!=NULL) {
-    // TODO(5): Usando un ciclo for inserta los datos en el arreglo
-    printf("[ ");
-    // TODO(6): Usando un ciclo for imprime la información en el arreglo
-    printf("]\n");
+    int *array, *array2;
+    int n;
 
-    // TODO(7): Asigna a array2, el arreglo duplicando su tamaño usando realloc
-     if (array2!=NULL) {
-        // TODO(8): Usando un ciclo for inserta los datos faltantes en el arreglo
-        printf("[ ");
-        // TODO(9): Usando un ciclo for imprime la información en el arreglo
+    printf("¿Cuantos elementos tiene el conjunto?\n");
+    scanf("%d", &n);
+    array = (int*)malloc(n*sizeof(int));
+
+    if (array != NULL){
+        for(int i=0; i<n; i++){
+        printf("Elemento %d: ", i+1);
+        scanf("%d", &array[i]);
+        }
+        printf("[");
+        for(int i=0; i<n; i++){
+        printf("%d ", array[i]);
+        }
         printf("]\n");
-      }
-    
-    // TODO(10): libera la memoria reservada por realloc
-  }
-  return 0;
+
+        array2 =(int*)realloc(array, (2*n)*sizeof(int));
+        if (array2 != NULL) {
+            for(int i=n; i < 2*n; i++){
+                printf("Elemento %d: ", i+1);
+                scanf("%d", &array2[i]);
+            }
+            printf("[");
+            for(int i=0; i < 2*n; i++){
+              printf("%d ", array2[i]);
+            }
+            printf("]\n");
+        }
+
+        free(array2);
+    }
+return 0;
 }
