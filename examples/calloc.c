5499ee243cf6f689a13916ee5f331f628285baed
Esquivel Cortés Iker Alonso <ikering226@gmail.com>
2026-03-09 12:43:16 -0600

    Update calloc.c

--- examples/calloc.c
+++ examples/calloc.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int main(){
+    int *array;
+    int num;
 
-int main (){
-  // TODO(1): Declara un apuntador a entero llamado array
-  // TODO(2): Declara una variable para indicar cuantos elementos se requieren
-  
-  printf("¿Cuantos elementos tiene el conjunto?\n");
-  // TODO(3): Lee el numero de elementos
-  // TODO(4): Usando una llamada a calloc reserva la memoria para el array
-  
-  if (array!=NULL) {
-    printf("[ ");
-    // TODO(5): Usando un ciclo for imprime la información en el arreglo
-    printf("]\n");
-    // TODO(6): libera la memoria reservada por malloc
-  }
-  return 0;
+    printf("¿Cuantos elementos tiene el conjunto\n");
+    scanf("%d", &num);
+    array = (int*)calloc(num, sizeof(int));
+
+    if(array!=NULL){
+        printf("[");
+        for(int i=0; i<num; i++)
+            printf("%d", array[i]);
+        printf("]\n");
+        free(array);
+    }
+    return 0;
 }
